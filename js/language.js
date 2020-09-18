d520e688d07785b4dd36c9f46f2b6916feab6fe2
josan <josanescod@gmail.com>
2020-09-18 11:17:34 +0200

    pruebas cambio_idioma menu y primera seccion

--- js/language.js
+++ js/language.js
@@ -8,6 +8,7 @@ window.onload = () => {
     seleccionaIdioma();
     pageInfo();
     
+    
 }
 
 function seleccionaIdioma() {
@@ -48,8 +49,9 @@ function cargaIdioma(file) {
                 }
                 // Examine the text in the response
                 response.json().then(function (data) {
-                    console.log(data);
-                    muestraDatos(data);
+                    
+                    
+                    muestraDatos(data,file);
                 });
             }
         )
@@ -61,12 +63,21 @@ function cargaIdioma(file) {
 
 }
 
-function muestraDatos(data) {
+function muestraDatos(data,file) {
     //Recorrer el objeto json 'data' y insertar en cada 'key' que corresponde a un elemento html su value que es el valor.
     Object.keys(data).forEach(function (key) {
         //console.table('key: '+ key + ', value: ' + data[key])
+        
+        if (key==='mot-nace'||key==='m-mot-nace'){
+            darEstilo(key,data[key],file)
+
+
+        }
+
+        else{
         let tempKey = document.querySelector(`#${key}`)
         tempKey.innerHTML = data[key]
+        }
     });
 }
 
@@ -82,7 +93,64 @@ function pageInfo(){
 function checkResolucion(){
     if (window.innerWidth < 960) {
         console.log('movil')
+        return 'movil'
     }else{
         console.log('escritorio')
+        return 'escritorio'
     }
+}
+
+/* tiene que llegar una variable con cat/cas porque los arrays
+tienen diferente longitud */
+function darEstilo(id,datos,file){
+            if (file==='es.json'){ //ES
+            //separar data[key] en 3 frases
+            let temp=datos.split(" ")
+            let p1=temp.slice(0,2).join(' ')
+            //console.log(p1)
+            let p2= temp.slice(2,12).join(' ')
+            //console.log(p2)
+            let p3= temp.slice(12,15).join(' ')
+            //console.log(p3)
+
+            if (checkResolucion()==='escritorio'){
+                let tempKey = document.querySelector(`#${id}`)
+                tempKey.innerHTML = `MOT nace <span class="uk-text-bold">para llevar el poder <br> transformador de
+                las palabras<br>
+                hasta ti</span>, estés donde estés.`
+            }else {
+                
+                let tempKey = document.querySelector(`#${id}`)
+                tempKey.innerHTML = `${p1} <span class="uk-text-bold">${p2}</span> ${p3}`
+            }    
+
+            
+
+            }else { //CAT
+            //separar data[key] en 3 frases
+            let temp=datos.split(" ")
+            let p1=temp.slice(0,2).join(' ')
+            //console.log(p1)
+            let p2= temp.slice(2,14).join(' ')
+            //console.log(p2)
+            let p3= temp.slice(14,17).join(' ')
+            //console.log(p3)
+
+            
+
+            if (checkResolucion()==='escritorio'){
+                
+                let tempKey = document.querySelector(`#${id}`)
+                tempKey.innerHTML = `MOT neix <span class="uk-text-bold">per a portar el poder <br> transformador de les paraules <br> fins a tu,</span> estiguis on estiguis.`
+            }else {
+               
+                let tempKey = document.querySelector(`#${id}`)
+                tempKey.innerHTML = `${p1} <span class="uk-text-bold">${p2}</span> ${p3}`
+            }    
+
+            
+
+            }
+            
+
 }
