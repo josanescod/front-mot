52e4e0e96aea58cbfe7ef8843fc7edd6879385d5
josan <josanescod@gmail.com>
2020-09-18 15:25:33 +0200

    cambio idioma bg7

--- js/language.js
+++ js/language.js
@@ -78,6 +78,8 @@ function muestraDatos(data,file) {
         let tempKey = document.querySelector(`#${key}`)
         tempKey.innerHTML = data[key]
         }
+
+
     });
 }
 
@@ -115,16 +117,41 @@ function darEstilo(id,datos,file){
 
             if (checkResolucion()==='escritorio'){
                 let tempKey = document.querySelector(`#${id}`)
+                //hardcoded se tendria que buscar mejorar solución
                 tempKey.innerHTML = `MOT nace <span class="uk-text-bold">para llevar el poder <br> transformador de
                 las palabras<br>
                 hasta ti</span>, estés donde estés.`
+
+                //harcoded bloque7
+                let texto_b7 = document.querySelector('#texto-b7');
+                texto_b7.innerHTML = `MOT es un <span class="uk-text-bold">estudio de palabras</span>.<br> Quizá te
+                resulte extraña esta definición,<br>
+                pero es un <span class="uk-text-bold">concepto nuevo que nace de creer<br> en el poder transformador de las palabras</span>. `
+
             }else {
                 
                 let tempKey = document.querySelector(`#${id}`)
                 tempKey.innerHTML = `${p1} <span class="uk-text-bold">${p2}</span> ${p3}`
+
+                //hardcoded bloque 7 se tendria que buscar mejorar solución
+                let texto_b7_m = document.querySelector('#texto-b7-m');
+                texto_b7_m=`MOT es un <span class="uk-text-bold">estudio de palabras</span>.Quizá te resulte extraña esta
+                definición, pero es un <span class="uk-text-bold">concepto nuevo que nace de creer en el poder
+                transformador de las palabras</span>.`
+
             }    
 
-            
+            //selecciona la clase bg2 y le añadimos a los dos div bg2.svg
+            let seccion2 = document.querySelectorAll('.bg2');
+            for (i=0;i<seccion2.length; i++){
+                seccion2[i].style.backgroundImage = `url('img/bg2.svg')`;
+            //selecciona logoyellow2 del bg7 y cambia el svg 
+            let logoseccion7 = document.querySelectorAll('.bg7-logo');
+            for (i=0;i<logoseccion7.length;i++){
+                logoseccion7[i].src='img/logoyellow2.svg';
+               
+            }   
+            }
 
             }else { //CAT
             //separar data[key] en 3 frases
@@ -142,12 +169,39 @@ function darEstilo(id,datos,file){
                 
                 let tempKey = document.querySelector(`#${id}`)
                 tempKey.innerHTML = `MOT neix <span class="uk-text-bold">per a portar el poder <br> transformador de les paraules <br> fins a tu,</span> estiguis on estiguis.`
+                //harcoded bloque7
+                let texto_b7 = document.querySelector('#texto-b7');
+                texto_b7.innerHTML = `MOT és un <span class="uk-text-bold">estudi de paraules</span>.<br> Potser et
+                resulta estranya aquesta definició,<br>
+                però és un <span class="uk-text-bold">un concepte nou que neix de creure<br> en el poder transformador de les paraules</span>. `
+            
+            
+            
             }else {
                
                 let tempKey = document.querySelector(`#${id}`)
                 tempKey.innerHTML = `${p1} <span class="uk-text-bold">${p2}</span> ${p3}`
-            }    
-
+                
+                //hardcoded bloque 7 se tendria que buscar mejorar solución
+                let texto_b7_m = document.querySelector('#texto-b7-m');
+                texto_b7_m.innerHTML=`MOT és un <span
+                class="uk-text-bold">estudi de paraules</span>.Potser et resulta estranya aquesta
+                definició, però és un <span class="uk-text-bold">concepte nou que neix de creure en el poder
+                transformador de l</span>.`
+            
+            
+            }   
+            
+            //selecciona la clase bg2 y le añadimos a os ldos div bg2_cat.svg
+            let seccion2 = document.querySelectorAll('.bg2');
+            for (i=0;i<seccion2.length; i++){
+                seccion2[i].style.backgroundImage = `url('img/bg2_cat.svg')`;
+            }
+            //selecciona logoyellow2 del bg7 y cambia el svg 
+            let logoseccion7 = document.querySelectorAll('.bg7-logo');
+            for (i=0;i<logoseccion7.length;i++){
+                logoseccion7[i].src='img/logoyellow2_cat.svg';
+            }   
             
 
             }
