6f4f27baab758d1a60796f648c06ed84f0af5241
Manuel Leyva <leyvamanu@gmail.com>
2020-10-29 22:49:50 +0100

    npm

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,20 @@
-var path;
+$(document).ready(function () {
+    (function () {
+        emailjs.init("user_eUy4bBvY6yrtUn0yEIaQJ");
+    })();
 
-window.onload = () => {
-    getPageHtml();
-}
+    var templateParams = {
+        name: 'James',
+        notes: 'Check this out!'
+    };
 
-function getPageHtml() {
-
-    path = window.location.pathname;
-    if (path === '/') {
-        let elements = document.querySelectorAll('#mot-opciones li');
-        let index=elements[0];
-        index.classList.add("uk-active");
-        index.lastChild.style.color = "#40a8ba";
-        
-    }
-    else if (path.includes('/html/')) {
-
-        path = path.substring(6);
-        changeColorMenuOption(path);
-
-    } else {
-
-        path = path.substring(1);
-        changeColorMenuOption(path);
-
-
-    }
-}
-
-function changeColorMenuOption(enlace) {
-    let ul = document.querySelectorAll('#mot-opciones li a');
-    
-    for (i = 0; i < ul.length; i++) {
-        let opcion = ul[i].getAttribute("href");
-
-        if (opcion.search(enlace) === 0) {
-
-            ul[i].parentNode.classList.add("uk-active");
-            ul[i].style.color = "#40a8ba";
-        }
-    }
-}
+    $(".mot-form").submit(function (event) {
+        event.preventDefault();
+        emailjs.send('gmail', 'contact_form', templateParams)
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
+});
