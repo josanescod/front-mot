35600028670f430eadc0d9fbef26a2d47c004322
josan <josanescod@gmail.com>
2020-10-30 15:24:15 +0100

    WIP:scrollanimation

--- js/scrollanimation.js
+++ js/scrollanimation.js
@@ -1,14 +1,23 @@
 $(function () {
-
-    $('button.mot-boton-azul-negro:first').on("click", function () {
-
+    $('button.mot-boton-azul-negro:first').on('click', function () {
+        reset();
         $('html, body').animate({
             scrollTop: $('div.uk-section').eq(2).offset().top
         }, 2000);
-
-        $('')
-
-
+        setTimeout(function () {
+            $('div.uk-section').eq(2).find('.uk-accordion-content').removeAttr('hidden');
+            $('div.uk-section').eq(2).find('a.uk-accordion-title').parent().addClass('uk-open');
+        }, 2700);
+        
     });
 
+
+    /*function reset(){
+        if (typeof $('div.uk-section').eq(2).find('.uk-accordion-content').attr('hidden')!== typeof undefined &&
+        typeof $('div.uk-section').eq(2).find('.uk-accordion-content').attr('hidden')!== false){
+            console.log('Elementos estaban ocultos')
+        }else {
+            console.log('Los elementos ya estan desplegados!')
+        }
+    }*/
 });
