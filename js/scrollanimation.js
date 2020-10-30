81e0618e9fe11b907c02927f4062be242cb9cb5d
josan <josanescod@gmail.com>
2020-10-30 15:40:19 +0100

    WIP:scrollanimation refactor

--- js/scrollanimation.js
+++ js/scrollanimation.js
@@ -1,6 +1,5 @@
 $(function () {
     $('button.mot-boton-azul-negro:first').on('click', function () {
-        reset();
         $('html, body').animate({
             scrollTop: $('div.uk-section').eq(2).offset().top
         }, 2000);
@@ -9,15 +8,5 @@ $(function () {
             $('div.uk-section').eq(2).find('a.uk-accordion-title').parent().addClass('uk-open');
         }, 2700);
         
-    });
-
-
-    /*function reset(){
-        if (typeof $('div.uk-section').eq(2).find('.uk-accordion-content').attr('hidden')!== typeof undefined &&
-        typeof $('div.uk-section').eq(2).find('.uk-accordion-content').attr('hidden')!== false){
-            console.log('Elementos estaban ocultos')
-        }else {
-            console.log('Los elementos ya estan desplegados!')
-        }
-    }*/
+    });    
 });
