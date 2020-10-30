835e6299e13d5544adf5939db15e8ae5de263a38
josan <josanescod@gmail.com>
2020-10-30 14:15:22 +0100

    WIP: scrollanimation en taller1 / F: .gitignore node_modules

---
+++ js/scrollanimation.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('button.mot-boton-azul-negro:first').on("click", function () {
+
+        $('html, body').animate({
+            scrollTop: $('div.uk-section').eq(2).offset().top
+        }, 2000);
+
+        $('')
+
+
+    });
+
+});
