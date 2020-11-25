23b839718f204d783602cb90414da9d874896cf7
Manuel Leyva <leyvamanu@gmail.com>
2020-11-25 21:07:57 +0100

    Recaptcha arreglado

--- js/custom.js
+++ js/custom.js
@@ -30,29 +30,29 @@ $(document).ready(function () {
 
         event.preventDefault();
 
+        var templateParams = {
+            form_type: $(this).find("[name=form_type]").val(),
+            tipo: $(this).find("[name=tipo]").val() ? 'Tipo: ' + $(this).find("[name=tipo]").val() : null,
+            dia: $(this).find("[name=dia]").val() ? 'Dia: ' + $(this).find("[name=dia]").val() : null,
+            horario: $(this).find("[name=horario]").val() ? 'Horario: ' + $(this).find("[name=horario]").val() : null,
+            inicio: $(this).find("[name=inicio]").val() ? 'Inicio: ' + $(this).find("[name=inicio]").val() : null,
+            duracion: $(this).find("[name=duracion]").val() ? 'Duracion: ' + $(this).find("[name=duracion]").val() : null,
+            nombre: $(this).find("[name=nombre]").val() ? 'Nombre: ' + $(this).find("[name=nombre]").val() : null,
+            apellidos: $(this).find("[name=apellidos]").val() ? 'Apellidos: ' + $(this).find("[name=apellidos]").val() : null,
+            email: $(this).find("[name=email]").val() ? 'Email: ' + $(this).find("[name=email]").val() : null,
+            telefono: $(this).find("[name=telefono]").val() ? 'Telefono: ' + $(this).find("[name=telefono]").val() : null,
+            edad: $(this).find("[name=edad]:checked").val() ? 'Edad: ' + $(this).find("[name=edad]:checked").val() : null,
+            profesion: $(this).find("[name=profesion]").val() ? 'Profesion: ' + $(this).find("[name=profesion]").val() : null,
+            localidad: $(this).find("[name=localidad]").val() ? 'Localidad: ' + $(this).find("[name=localidad]").val() : null,
+            conocido: $(this).find("[name=conocido]:checked").val() ? 'Ha conocido Mot por: ' + $(this).find("[name=conocido]:checked").val() : null,
+            conocidoOtro: $(this).find("[name=conocido-otro]").val() ? 'Como fue: ' + $(this).find("[name=conocido-otro]").val() : null,
+            mensaje: $(this).find("[name=mensaje]").val() ? 'Mensaje: ' + $(this).find("[name=mensaje]").val() : null
+        };
+
+        var typeForm = $(this).find("[data-type-form]").attr('data-type-form');
+
         grecaptcha.ready(function () {
             grecaptcha.execute('6LcmdcMUAAAAADUtZa8MRDNOSUk61KxXb0I_iUvb', {action: 'submit'}).then(function (token) {
-
-                var templateParams = {
-                    form_type: $(this).find("[name=form_type]").val(),
-                    tipo: $this.find("[name=tipo]").val() ? 'Tipo: ' + $this.find("[name=tipo]").val() : null,
-                    dia: $this.find("[name=dia]").val() ? 'Dia: ' + $this.find("[name=dia]").val() : null,
-                    horario: $this.find("[name=horario]").val() ? 'Horario: ' + $this.find("[name=horario]").val() : null,
-                    inicio: $this.find("[name=inicio]").val() ? 'Inicio: ' + $this.find("[name=inicio]").val() : null,
-                    duracion: $this.find("[name=duracion]").val() ? 'Duracion: ' + $this.find("[name=duracion]").val() : null,
-                    nombre: $this.find("[name=nombre]").val() ? 'Nombre: ' + $this.find("[name=nombre]").val() : null,
-                    apellidos: $this.find("[name=apellidos]").val() ? 'Apellidos: ' + $this.find("[name=apellidos]").val() : null,
-                    email: $this.find("[name=email]").val() ? 'Email: ' + $this.find("[name=email]").val() : null,
-                    telefono: $this.find("[name=telefono]").val() ? 'Telefono: ' + $this.find("[name=telefono]").val() : null,
-                    edad: $this.find("[name=edad]:checked").val() ? 'Edad: ' + $this.find("[name=edad]:checked").val() : null,
-                    profesion: $this.find("[name=profesion]").val() ? 'Profesion: ' + $this.find("[name=profesion]").val() : null,
-                    localidad: $this.find("[name=localidad]").val() ? 'Localidad: ' + $this.find("[name=localidad]").val() : null,
-                    conocido: $this.find("[name=conocido]:checked").val() ? 'Ha conocido Mot por: ' + $this.find("[name=conocido]:checked").val() : null,
-                    conocidoOtro: $this.find("[name=conocido-otro]").val() ? 'Como fue: ' + $this.find("[name=conocido-otro]").val() : null,
-                    mensaje: $this.find("[name=mensaje]").val() ? 'Mensaje: ' + $this.find("[name=mensaje]").val() : null
-                };
-
-                var typeForm = $this.find("[data-type-form]").attr('data-type-form');
 
                 emailjs.send('gmail', 'forms', templateParams)
                     .then(function (response) {
